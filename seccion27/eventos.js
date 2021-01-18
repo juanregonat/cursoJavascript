a95a7759cef1f76e1e36c66f089dc637afafe664
juan regonat <juanregonat@gmail.com>
2021-01-18 06:36:08 -0300

    18 enero

---
+++ seccion27/eventos.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+    //alert('listo');
+
+    var caja = $('#caja');
+console.log(caja);
+
+caja.mouseover(function (){
+    $(this).css("background","red");
+});
+//ffcc00
+//chartreuse
+caja.mouseout(function (){
+    $(this).css("background","chartreuse");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
