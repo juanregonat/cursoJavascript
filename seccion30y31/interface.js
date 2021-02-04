46928ea30fed755a3e1802d4d4e190f380e4823b
juan regonat <juanregonat@gmail.com>
2021-02-03 21:23:22 -0300

    fin secciones 30 y 31

---
+++ seccion30y31/interface.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    //alert("SDF");
+
+    //mover un elemento por la página: 
+    $('.elemento').draggable();
+
+    //cambiar de tamaño de un elemento: 
+    $('.elemento').resizable();
+
+    //elegir elementos de una lista y ordenarlos:
+    //$('.lista-seleccionable').selectable();
+    $('.lista-seleccionable').sortable({
+        update: function (event,ui) {
+            console.log("ha cambiado la lista");
+        }
+    });
+
+    //drop:
+    $('#elemento-movido').draggable();
+    $('#area').droppable({
+        drop: function(){
+            console.log("has soltado algo");
+            //console.log("has movido " + $(this).attr('id') );
+        }
+    });
+
+
+
+
+
+
+    
+
+});
