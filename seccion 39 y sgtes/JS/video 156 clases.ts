c71913fafec3b280e2797093144ee1de824f9706
juan regonat <juanregonat@gmail.com>
2021-03-10 23:44:26 -0300

    ts

---
+++ seccion 39 y sgtes/JS/video 156 clases.ts
@@ -0,0 +1,25 @@
+class Camiseta{
+
+    //propiedades
+    private modelo:string;
+    private color:string;
+    private marca:string;
+    private talla:string;
+    private precio:string;
+
+    //metodos
+
+    public setColor(color){
+        this.color=color;
+    }
+
+    public getColor(){
+        return this.color;
+    }
+
+}
+
+var camiseta = new Camiseta();
+camiseta.setColor("rojo");
+
+console.log(camiseta.getColor());
