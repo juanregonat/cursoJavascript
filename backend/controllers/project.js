eeda5c369f5906bcc5d353add5763edbba8cfb0c
juan regonat <juanregonat@gmail.com>
2021-05-30 20:15:09 -0300

    abm node

--- backend/controllers/project.js
+++ backend/controllers/project.js
@@ -36,11 +36,93 @@ var controller = {
                 message : 'No se ha podido guardar'
             });
             return res.status(200).send({project:projectStored});
+        });
+    },
+
+    getProject: function(req, res){
+        var projectId = req.params.id;
+
+        if(projectId == null) res.status(404).send({
+                 message: "El proyecto es nulo"
         })
 
-    }
+        Project.findById(projectId,(err,project)=>{
+            if(err) return res.status(500).send({
+                message: "Error al devolver los datos"
+            })
 
-};
+            if(!project) return res.status(404).send({
+                message: "El proyecto no existe"
+            })
+
+            return res.status(200).send({
+                project
+            });
+
+        });
+            
+    },
+
+    getProjects: function(req, res){
+
+        Project.find({}).sort('-year').exec((err, projects) => {
+            if(err) return res.status(500).send({
+                message: "Error 500"
+            });
+
+            if(!projects) return res.status(404).send({
+                message: "No hay proyectos"
+            });
+
+            return res.status(200).send({
+                projects
+            });
+
+        })
+    },
+
+    updateProject: function(req, res){
+        var projectId = req.params.id;
+        var update = req.body;
+
+        // parametros: el id a actualizar, los datos actualizados, forzar actualizacion, callback de error o exito
+        Project.findByIdAndUpdate(projectId, update, {new:true}, (err, projectUpdated)=>{
+            if(err) return res.status(500).send({
+                message: "Error al actualizar"
+            });
+
+            if(!projectUpdated) return res.status(404).send({
+                message: "El proyecto no existe"
+            });
+
+            return res.status(200).send({
+                project: projectUpdated
+            });
+
+        });
+    },
+
+    deleteProject: function(req, res){
+        var projectId = req.params.id;        
+
+        // parametros: el id a actualizar, los datos actualizados, forzar actualizacion, callback de error o exito
+        Project.findOneAndDelete (projectId, (err, projectDeleted)=>{
+            if(err) return res.status(500).send({
+                message: "Error al eliminar"
+            });
+
+            if(!projectDeleted) return res.status(404).send({
+                message: "El proyecto no existe"
+            });
+
+            return res.status(200).send({
+                project:projectDeleted
+            });
+
+        });            
+    },
+
+
+};  
 
 module.exports = controller;
-
